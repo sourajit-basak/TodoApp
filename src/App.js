d4961ce642ca14ea2f342af08e708ee427b37a24
Sourajit <sourajit.basak@nordcloud.com>
2022-03-11 14:00:24 +0530

    mui changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import React, { useState, useRef, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import {
+  Box,
+  Button,
+  Input,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+} from "@mui/material";
+
 function App() {
   const [todos, setTodos] = useState([]);
   const todoText = useRef();
@@ -10,6 +20,8 @@ function App() {
     const next = [...todos, todoText.current.value];
     setTodos(next);
     localStorage.setItem("todos", JSON.stringify(next));
+    console.log(todoText.current.value);
+    todoText.current.value = "";
   };
   useEffect(() => {
     const existingTodos = localStorage.getItem("todos");
@@ -17,15 +29,36 @@ function App() {
   }, []);
   return (
     <div>
-      <ul>
+      {/* <ul>
         {todos.map((todo) => (
           <li key={todo}>{todo}</li>
         ))}
-      </ul>
-      <form onSubmit={addTodo}>
-        <input ref={todoText}></input>
-        <input type="submit" value="Add Todo" />
-      </form>
+      </ul> */}
+      <Box sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+        <List>
+          {todos.map((todo) => (
+            <ListItem key={todo} disablePadding>
+              {/* <ListItemText primary="Inbox" /> */}
+              <ListItemButton>
+                <ListItemText primary={todo} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+      <Box sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+        <form onSubmit={addTodo}>
+          <div>
+            {" "}
+            {/* <input ref={todoText}></input> */}
+            <Input placeholder="Enter To do" inputRef={todoText}></Input>
+            {/* <input type="submit" value="Add Todo" /> */}
+            <Button variant="outlined" onSubmit={addTodo}>
+              Add Todo
+            </Button>
+          </div>
+        </form>
+      </Box>
     </div>
   );
 }
